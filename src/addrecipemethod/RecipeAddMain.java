af13fea19c542aa839b47b3908a8b2ee9406afe8
SullyC76 <sullcallahan23@gmail.com>
2026-03-13 17:21:03 -0400

    Added Button

---
+++ src/addrecipemethod/RecipeAddMain.java
@@ -0,0 +1,9 @@
+package addrecipemethod;
+
+public class RecipeAddMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");	}
+
+}
---
+++ src/addrecipemethod/RecipeAddTest.java
@@ -0,0 +1,17 @@
+package addrecipemethod;
+import java.util.Scanner;
+
+public class RecipeAddTest {
+	
+	
+	
+}
+
+
+/**public Recipe addRecipe(){
+ * This method will prompt the user to input a title, description, 
+ * any number of ingredients and any number of steps to create a recipe
+ * 
+ * It will use the constructor from the recipe class to create said recipe and will return the recipe when the method has concluded
+	
+}*/
